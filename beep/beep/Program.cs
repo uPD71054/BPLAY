adf22b6252704fe5f68041d7fb4a5351aeeca768
i3 <s1213052@gmail.com>
2017-11-11 23:51:56 +0900

    use delegate for callback

--- beep/beep/Program.cs
+++ beep/beep/Program.cs
@@ -12,15 +12,26 @@ namespace slimDXTestConsole
 {
     class Program
     {
+        static int cc = 0;
+        static char[] bars = { '／', '―', '＼', '｜' };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("beep ver0.2.2");
+            Console.WriteLine("beep ver0.2.3");
             xaudio beep = new xaudio();
-            Console.WriteLine("Press ESC to exit...");
+            beep.bufferEndProc = callback;
+            Console.Write("Press ESC to exit...");
             do
             {
                 // process
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            Console.WriteLine();
+        }
+
+        static private void callback()
+        {
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write("{0}Press ESC to exit...", bars[cc++ % 4]);
         }
     }
 
@@ -28,10 +39,10 @@ namespace slimDXTestConsole
     {
         // Const parametor
         private const int SAMPLERATE = 44100 /* kHz */;
-        private const int BITDEPTH = 8 /* 8bit/sample */;
+        private const int BITDEPTH = 16/* 8bit/sample */;
         private const int CHANNELS = 1 /* monoral */;
-        private const int NUMOFBUF = 4;
-        private const int NUMOFSAMPLE = 882 * 2;
+        private const int NUMOFBUF = 2;
+        private const int NUMOFSAMPLE = 882;
 
         internal XAudio2 device;
         internal MasteringVoice masteringVoice;
@@ -40,78 +51,83 @@ namespace slimDXTestConsole
         internal AudioBuffer[] audioBuffer;
         internal byte[][] data;
         internal int bufferCount;
-        internal byte[] b = new byte[NUMOFSAMPLE];
+
+        public delegate void audioCallback();
+        public audioCallback bufferEndProc;
+
+
+
         public xaudio()
         {
-            for (int i = 0; i < b.Length; i++)
-            {
-                b[i] = pulse(880);
-            }
-            audioInit();
-
-            sourceVoice.BufferEnd += sourceVoice_BufferEnd;
-            //sourceVoice.StreamEnd += bufferEnd;
-        }
-
-        private void audioInit()
-        {
-
             device = new XAudio2();
             masteringVoice = new MasteringVoice(device);
 
             waveFormat = new WaveFormat();
-            waveFormat.Channels = CHANNELS;
-            waveFormat.SamplesPerSecond = SAMPLERATE;
-            waveFormat.BitsPerSample = BITDEPTH;
-            waveFormat.BlockAlignment = BITDEPTH / 8 * CHANNELS;
-            waveFormat.AverageBytesPerSecond = SAMPLERATE * waveFormat.BlockAlignment;
-            waveFormat.FormatTag = WaveFormatTag.Pcm;
+            {
+                waveFormat.Channels = CHANNELS;
+                waveFormat.SamplesPerSecond = SAMPLERATE;
+                waveFormat.BitsPerSample = BITDEPTH;
+                waveFormat.BlockAlignment = (short)(waveFormat.BitsPerSample / 8 * waveFormat.Channels);
+                waveFormat.AverageBytesPerSecond = waveFormat.SamplesPerSecond * waveFormat.BlockAlignment;
+                waveFormat.FormatTag = WaveFormatTag.Pcm;
+            }
 
             sourceVoice = new SourceVoice(device, waveFormat);
+            sourceVoice.BufferEnd += sourceVoice_BufferEnd;
 
             audioBuffer = new AudioBuffer[NUMOFBUF];
             data = new byte[NUMOFBUF][];
             for (int i = 0; i < NUMOFBUF; i++)
             {
-                data[i] = new byte[NUMOFSAMPLE];
+                data[i] = new byte[NUMOFSAMPLE * waveFormat.BlockAlignment];
+                for (int j = 0; j < data[i].Length; j += waveFormat.BlockAlignment)
+                {
+                    switch (waveFormat.Channels | waveFormat.BitsPerSample)
+                    {
+                        case 9:
+                            data[i][j] = pulse(2000 - 1000 * i);
+                            break;
+                        case 10:
+                            data[i][j + 0] = data[i][j + 1] = pulse(2000 - 1000 * i);
+                            break;
+                        case 17:
+                            data[i][j] = 0x00;
+                            data[i][j + 1] = pulse(2000 - 1000 * i);
+                            break;
+                        case 18:
+                            data[i][j + 0] = data[i][j + 2] = 0x00;
+                            data[i][j + 1] = data[i][j + 3] = pulse(2000 - 1000 * i); ;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
                 audioBuffer[i] = new AudioBuffer();
                 audioBuffer[i].AudioData = new MemoryStream(data[i], true);
                 audioBuffer[i].Flags = BufferFlags.EndOfStream;
-                audioBuffer[i].AudioBytes = NUMOFSAMPLE;
+                audioBuffer[i].AudioBytes = data[i].Length;
                 audioBuffer[i].LoopCount = 0;
-            }
 
-            for (int j = 0; j < NUMOFSAMPLE; j++)
-            {
-                data[0][j] = pulse(2000);
+                audioBuffer[i].AudioData.Position = 0;
+                sourceVoice.SubmitSourceBuffer(audioBuffer[i]);
             }
-            audioBuffer[0].AudioData.Position = 0;
-            sourceVoice.SubmitSourceBuffer(audioBuffer[0]);
-            for (int j = 0; j < NUMOFSAMPLE; j++)
-            {
-                data[1][j] = pulse(1000);
-            }
-            audioBuffer[1].AudioData.Position = 0;
-            sourceVoice.SubmitSourceBuffer(audioBuffer[1]);
             bufferCount = 0;
-
             sourceVoice.Start();
         }
 
-        int callCount = 0;
         void sourceVoice_BufferEnd(object sender, ContextEventArgs e)
         {
-            Console.WriteLine("called!");
-
-            for (int i = 0; i < NUMOFSAMPLE; i++)
+            for (int i = 0; i < data[bufferCount].Length; i++)
             {
                 data[bufferCount][i] = 0;
             }
-            callCount += 16;
+
             audioBuffer[bufferCount].AudioData.Position = 0;
             sourceVoice.SubmitSourceBuffer(audioBuffer[bufferCount]);
             bufferCount = ++bufferCount & 0x01;
-            Console.WriteLine("buffer {0}", bufferCount);
+
+            bufferEndProc();
         }
 
 
@@ -123,17 +139,12 @@ namespace slimDXTestConsole
             phase -= Math.Floor(phase);
             if (phase > 0.5)
             {
-                return Byte.MaxValue >> 2;
+                return Byte.MaxValue >> 1;
             }
             else
             {
                 return 0;
             }
-        }
-
-        internal void streamend()
-        {
-            Console.WriteLine("stream end!");
         }
 
     }
